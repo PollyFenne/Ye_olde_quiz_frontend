508a7e8879d8e4051aeb7deca0baeb9a16d09eca
adamminchella <adamminchella@msn.com>
2023-01-16 09:58:43 +0000

    Create ButtonControl component and add to HomePage

---
+++ src/components/ButtonControl/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const ButtonControl = ({ buttonText }) => {
+  return <button className="create-game-button">{buttonText}</button>;
+};
+
+export default ButtonControl;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,24 @@
-// HOMEPAGE 
+// HOMEPAGE
 
 import React from "react";
 
-import Banner from "../../components/Banner"
-import './Home.css'
+import Banner from "../../components/Banner";
+
+import ButtonControl from "../../components/ButtonControl";
+
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <>
-        <Banner />
-        <div className="home-content">
-            <h1>Homepage</h1>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <Banner />
+      <div className="home-content">
+        <h1>Homepage</h1>
+        <ButtonControl buttonText={"NEW QUIZ"} />
+        <ButtonControl buttonText={"JOIN QUIZ"} />
+      </div>
+    </>
+  );
 };
 
 export default Home;
