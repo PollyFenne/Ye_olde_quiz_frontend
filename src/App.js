f801ea6db67b11ee0a4cb22c6cd2f6fb5862f049
Polly <pollyannafenne@gmail.com>
2023-01-16 10:36:01 +0000

    Adding login functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 // App.js
 
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import { useState } from "react";
 
-import Home from "./pages/Home";
+import { Home } from "./pages";
+import Login from "./components/Login.js";
+import useToken from "./components/useToken";
+
 
 const App = () => {
+
+    const { token, setToken } = useToken();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+
     return (
         <>
-            <Home />
+            <Routes>
+                <Route exact path='/' element={ <Home /> } />
+            </Routes>
         </>
     )
 };
