10a43bc5412c9af5745d327d0fff0a3d53b421c6
JLP2000 <josh15phan@gmail.com>
2023-01-18 22:31:24 +0000

    removed full stop from modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./styles.css";
 
 const Modal = ({ waiting }) => {
+  console.log(waiting)
+
   return (
     <>
       <div className="modal">
         <h2>Thanks for submitting you answers</h2>
-        <h3>{waiting}.</h3>
+        <h3>{waiting}</h3>
       </div>
     </>
   );
