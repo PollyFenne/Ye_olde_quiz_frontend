61bfebe583c4956a1bbe2cebb6153f2c3459000b
Polly <pollyannafenne@gmail.com>
2023-01-19 15:17:50 +0000

    Some additonal modal styling

--- src/components/ShowResultModal/index.js
+++ src/components/ShowResultModal/index.js
@@ -1,12 +1,16 @@
+// Results are each round
+
 import React from "react";
+import './styles.css'
 
 const ShowResultModal = ({ scores, finalResults }) => {
   console.log(scores, finalResults);
 
   return (
-    <div className="modal">
+    <div className="round-results-modal">
       {finalResults ?? <h1>Final Results are:</h1>}
-      <ul>
+      <h2>Score after that round:</h2>
+      <ul className="round-result-score">
         {scores.map((score, i) => (
           <li key={i}>
             {score.username}: {score.userscore}
