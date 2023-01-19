6042124dbc3a70d73733c1643208f5b070c2a0ba
Polly <pollyannafenne@gmail.com>
2023-01-19 17:31:26 +0000

    styling of modal

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -7,8 +7,8 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>Sorry, page not found</h2>
-      <img src="../../assets/images/Logo1.png" alt="Logo" />
+      <h2>Sorry, pub not found</h2>
+      <img src="../../assets/images/Logo_spilled_done.png" alt="Logo" />
     </div>
   );
 };
